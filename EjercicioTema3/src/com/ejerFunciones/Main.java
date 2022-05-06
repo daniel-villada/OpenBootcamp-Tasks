c8d8e14eca9b3a45ce45d58598b89aef3cbbd7ef
daniel-villada <danielvillada980@gmail.com>
2022-05-05 20:10:39 -0500

    Primera parte

--- EjercicioTema3/src/com/ejerFunciones/Main.java
+++ EjercicioTema3/src/com/ejerFunciones/Main.java
@@ -1,7 +1,19 @@
 package com.ejerFunciones;
 
+
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hola Mundo");
+    public static void main(String[] args)
+    {
+        int resultado = 0;
+        resultado = suma(2, 5, 3);
+        System.out.println("El resultado de la suma es: " + resultado);
+    }
+    public static int suma(int a, int b, int c){
+        return a + b + c;
     }
 }
+
+
+
+
