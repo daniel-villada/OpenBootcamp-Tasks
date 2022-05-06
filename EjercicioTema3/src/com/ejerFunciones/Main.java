b186b032856aad3907a91c3d738a68ad8b5208d6
daniel-villada <danielvillada980@gmail.com>
2022-05-05 21:49:16 -0500

    Finalizo el Ejercicio ( funcion suma, Clase Coche)

--- EjercicioTema3/src/com/ejerFunciones/Main.java
+++ EjercicioTema3/src/com/ejerFunciones/Main.java
@@ -3,25 +3,34 @@ package com.ejerFunciones;
 
 
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        //llamo mi funcion suma
         suma(2, 5, 3);
+
+        //Creo Objecto miCoche
+        Coche miCoche = new Coche();
+        miCoche.numeroPuertas = 4;
+        System.out.println("Numero de puertas: " + miCoche.incrementoPuertas(1));
     }
 
-    //Primera parte del ejercicio: Funcion que sume 3 numeros.
+    //Primera parte del ejercicio: Funcion que suma 3 numeros.
     public static void suma(int a, int b, int c){
         int resultado;
         resultado = a + b + c;
         System.out.println("El resultado de la suma es: " + resultado);
     }
-}
 
-class Coche{
-    int numeroPuertas = 4;
-    int puertasCoche(int numeroPuertas,int nPuertas){
-        return numeroPuertas + nPuertas;
+    //Segunda parte del ejercicio: Creo Clase Coche
+    public static class Coche{
+        int numeroPuertas;
+
+        int incrementoPuertas(int puerta){
+            int cantidadPuertas = numeroPuertas + puerta;
+            return cantidadPuertas;
+        }
     }
 }
+
 
 
 
