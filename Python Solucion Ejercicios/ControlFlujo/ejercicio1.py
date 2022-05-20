e6593a01b38fb19c2be05e827a26d01b1e3ef501
daniel-villada <danielvillada980@gmail.com>
2022-05-19 21:03:14 -0500

    IDK

---
+++ Python Solucion Ejercicios/ControlFlujo/ejercicio1.py
@@ -0,0 +1,9 @@
+
+#Escribe un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
+edadUsuario = int(input("Ingrese su edad: "))
+
+if(edadUsuario >= 18):
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
