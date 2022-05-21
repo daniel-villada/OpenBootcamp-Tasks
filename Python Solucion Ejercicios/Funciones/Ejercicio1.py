14d166de756518782cc23d78b6c05bd7890000af
daniel-villada <danielvillada980@gmail.com>
2022-05-20 23:21:49 -0500

    Solucion Ejercicio 1 - Funciones

--- Python Solucion Ejercicios/Funciones/Ejercicio1.py
+++ Python Solucion Ejercicios/Funciones/Ejercicio1.py
@@ -2,14 +2,14 @@
 #Escribe una función que calcule el área de un triángulo, recibiendo la altura y la base como parámetros
 # y otra función que calcule el área de un círculo recibiendo el radio del mismo.
 
+import math
+
 def areaTriangulo(altura, base):
-    return base * altura / 2
+    return ((base * altura) / 2)
 
 print(f"El area del triangulo es: {areaTriangulo(10,20)}")
 
-def areaCirculo(diametro):
-    radio =  diametro / 2
-    pi = 3.14
-    return pi * radio ** 2
+def areaCirculo(radio):
+    return (math.pi * radio ** 2)
 
-print(f"El area del circulo es: {areaCirculo(38)}")
+print(f"El area del circulo es: {areaCirculo(19)}")
