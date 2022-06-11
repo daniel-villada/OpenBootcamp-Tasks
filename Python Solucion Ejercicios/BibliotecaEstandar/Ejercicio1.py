b4fe4b34d4b094bd24c825c852aea95558c6b7b5
daniel-villada <danielvillada980@gmail.com>
2022-06-10 22:14:37 -0500

    Solucion Ejercicio1 Biblioteca Estandar

---
+++ Python Solucion Ejercicios/BibliotecaEstandar/Ejercicio1.py
@@ -0,0 +1,16 @@
+#Crea un script que le pida al usuario una lista de países (separados por comas).
+# Éstos se deben almacenar en una lista.
+# No debería haber países repetidos (haz uso de set). Finalmente,
+# muestra por consola la lista de países ordenados
+# alfabéticamente y separados por comas.
+
+userList = input("Introduce una lista de países separados por comas: ")
+
+countryList = [country for country in userList.split(",")]
+
+print(",".join(sorted(list(set(countryList)))))
+
+
+
+
+
