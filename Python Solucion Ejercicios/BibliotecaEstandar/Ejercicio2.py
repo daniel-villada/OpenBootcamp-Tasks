1ac37971e1b312b5fb6a9969ef0547622601d0e2
daniel-villada <danielvillada980@gmail.com>
2022-06-10 22:20:30 -0500

    Solucion Ejercicio 2 Biblioteca Estandar

--- Python Solucion Ejercicios/BibliotecaEstandar/Ejercicio2.py
+++ Python Solucion Ejercicios/BibliotecaEstandar/Ejercicio2.py
@@ -1,2 +1,16 @@
 #En este segundo ejercicio, tenéis que crear una aplicación que obtendrá los elementos impares de una lista pasada por
 # parámetro con filter y realizará una suma de todos estos elementos obtenidos mediante reduce.
+
+
+from functools import reduce
+
+def getImpares(lista):
+    result = list(filter((lambda x: x % 2), listNumbers))
+    print(result)
+
+    result = reduce((lambda x, y: x + y), result)
+    print(result)
+
+listNumbers = list(range(100))
+
+getImpares(list)
